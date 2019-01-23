dac10f72aa147606e38ca79ad6dc867fb009560c
jonnaPan <297544049@qq.com>
2019-01-23 09:07:12 +0800

    11

--- siteinfo.js
+++ siteinfo.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: "鑫业同城",
-  //siteroot: "https://vshop.chinaxywl.com/", // 必填: api地址，结尾要带/
+  siteroot: "https://vshop.chinaxywl.com/", // 必填: api地址，结尾要带/
   //siteroot: "https://panqing1994.utools.club/htdocs/vshop/web/",
-  siteroot: "http://192.168.0.108/htdocs/vshop/web/",
+  //siteroot: "http://192.168.0.108/htdocs/vshop/web/",
   uniacid: "10001", // 默认即可，勿填
 };
